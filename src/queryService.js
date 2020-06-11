efa26888558187dafba0dd2e07aa25f83a2b827d
Robin Denton <raenton@gmail.com>
2020-06-11 11:19:53 +0100

    add stubbed queryservice

---
+++ src/queryService.js
@@ -0,0 +1,15 @@
+class QueryService {
+  constructor() {
+
+  }
+
+  async exec(query) {
+    return {
+      ubermensch: 'nowhere to be found'
+    }
+  }
+}
+
+module.exports = {
+  QueryService
+}
