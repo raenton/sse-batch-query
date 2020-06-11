5eb170206dd89f507cab658a859d6e836569a9d9
Robin Denton <raenton@gmail.com>
2020-06-11 11:21:28 +0100

    Add queryService and new listeners to QueryQueue

--- src/context.js
+++ src/context.js
@@ -2,13 +2,21 @@ const {
   QueryQueue,
   QueryEvents
 } = require('./QueryQueue.js')
+const { QueryService } = require('./queryService')
 const streamService = require('./streamService')
 
 exports.createContext = () => {
+  const queryService = new QueryService()
+
   // instantiate a queue to push query demands to
-  const queryQueue = new QueryQueue()
+  const queryQueue = new QueryQueue(queryService)
+
   // listen for query completion events and write messages to stream
   queryQueue.addListener(QueryEvents.QUERY_COMPLETE, streamService.writeMessage)
+  // listen for query errors and write to error stream
+  queryQueue.addListener(QueryEvents.QUERY_ERROR, streamService.writeError)
+  // listen for empty queue events and do some other stuff
+  // queryQueue.addListener(QueryEvents.QUEUE_EMPTY, doStuff)
   
   return {
     streamService,
